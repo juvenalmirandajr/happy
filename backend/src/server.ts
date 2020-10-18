8aef41c277c44ba18b6bb2ce8ccbd30238a31538
jumiranda <juvenal.miranda@cengage.com>
2020-10-18 15:36:24 -0400

    Backend structure implemented

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/users", (req, res) => {
+  return res.json({ message: "Hi" });
+});
+
+app.listen(3333);
