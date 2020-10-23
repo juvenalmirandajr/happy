ccc10be43ee9e414a48f478f25a013bf0ed48cdb
jumiranda <juvenal.miranda@cengage.com>
2020-10-22 23:31:21 -0400

    added images upload

--- backend/src/database/migrations/1603422156110-create_orphanages.ts
+++ backend/src/database/migrations/1603422156110-create_orphanages.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class createOrphanages1603051197923 implements MigrationInterface {
+export class createOrphanages1603422156110 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
